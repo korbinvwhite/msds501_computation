7ac288fc6178780bac95cfe1f5fa76fcb9348fb8
Elias Ghiz <eeghiz@dons.usfca.edu>
2026-06-30 14:14:01 -0700

    fixed the issue with line 36

--- partner_b.py
+++ partner_b.py
@@ -33,9 +33,16 @@ scores = ['88', '95', 'absent', '72', 'n/a', '84']
 valid = []
 
 for i, s in enumerate(scores):
-    score = int(s)            # <-- this line crashes on bad strings
-    print(f'Score [{i}]: {score}')
-    valid.append(score)
+    try:
+        score = int(s)            # <-- this line crashes on bad strings
+        print(f'Score [{i}]: {score}')
+        valid.append(score)
+    except ValueError:
+        print(f"[index {i}] Skipped bad score: '{s}'")
+
+
 
 print(f'Total of valid scores: {sum(valid)}')
 print(f'Count of valid scores: {len(valid)}')
+
+#Task 2 the error is a ValueError(TypeError) on line 36. The reason for the error is it is trying to read a string as a int
